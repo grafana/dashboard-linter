cfb9f01d7a7683bdb51aa58f9227885e3666e534
Muhammad Shahzeb <mhmdshahzeb1993@gmail.com>
2022-06-23 01:09:51 +0500

    Initial commit

--- lint/lint.go
+++ lint/lint.go
@@ -158,11 +158,13 @@ type Target struct {
 // Panel is a deliberately incomplete representation of the Dashboard -> Panel type in grafana.
 // The properties which are extracted from JSON are only those used for linting purposes.
 type Panel struct {
-	Title      string     `json:"title"`
-	Targets    []Target   `json:"targets,omitempty"`
-	Datasource Datasource `json:"datasource"`
-	Type       string     `json:"type"`
-	Panels     []Panel    `json:"panels,omitempty"`
+	Id          int        `json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	Targets     []Target   `json:"targets,omitempty"`
+	Datasource  Datasource `json:"datasource"`
+	Type        string     `json:"type"`
+	Panels      []Panel    `json:"panels,omitempty"`
 }
 
 // GetPanels returns the all panels nested inside the panel (inc the current panel)
