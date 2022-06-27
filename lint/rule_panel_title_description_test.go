401a71778ae040485aba80aa000cbc1a3f596aca
Muhammad Shahzeb <mhmdshahzeb1993@gmail.com>
2022-06-28 03:13:07 +0500

    Add all failure test cases

--- lint/rule_panel_title_description_test.go
+++ lint/rule_panel_title_description_test.go
@@ -19,9 +19,32 @@ func TestPanelTitleDescription(t *testing.T) {
 			panel: Panel{
 				Type:        "singlestat",
 				Id:          1,
-				Datasource:  "foo",
 				Title:       "",
 				Description: "",
+			},
+		},
+		{
+			result: Result{
+				Severity: Error,
+				Message:  "Dashboard 'test', panel with id '2' has missing title or description, currently has title 'title' and description: ''",
+			},
+			panel: Panel{
+				Type:        "singlestat",
+				Id:          2,
+				Title:       "title",
+				Description: "",
+			},
+		},
+		{
+			result: Result{
+				Severity: Error,
+				Message:  "Dashboard 'test', panel with id '3' has missing title or description, currently has title '' and description: 'description'",
+			},
+			panel: Panel{
+				Type:        "singlestat",
+				Id:          3,
+				Title:       "",
+				Description: "description",
 			},
 		},
 		{
