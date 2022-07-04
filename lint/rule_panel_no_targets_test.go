e9ec5cfac876b9a033fda40bcec097bca6d25c10
Muhammad Shahzeb <mhmdshahzeb1993@gmail.com>
2022-07-05 03:53:32 +0500

    Initial commit

---
+++ lint/rule_panel_no_targets_test.go
@@ -0,0 +1,44 @@
+package lint
+
+import (
+	"testing"
+)
+
+func TestPanelNoTargets(t *testing.T) {
+	linter := NewPanelNoTargetsRule()
+
+	for _, tc := range []struct {
+		result Result
+		panel  Panel
+	}{
+		{
+			result: Result{
+				Severity: Error,
+				Message:  "Dashboard 'test', panel 'bar' has no targets",
+			},
+			panel: Panel{
+				Type:       "singlestat",
+				Datasource: "foo",
+				Title:      "bar",
+			},
+		},
+		{
+			result: Result{
+				Severity: Success,
+				Message:  "OK",
+			},
+			panel: Panel{
+				Type:       "singlestat",
+				Datasource: "foo",
+				Title:      "bar",
+				Targets: []Target{
+					{
+						Expr: `sum(rate(foo[5m]))`,
+					},
+				},
+			},
+		},
+	} {
+		testRule(t, linter, Dashboard{Title: "test", Panels: []Panel{tc.panel}}, tc.result)
+	}
+}
