77e71424182cd225917326dc380c1bff56c955fb
Vitaly Zhuravlev <v-zhuravlev@users.noreply.github.com>
2024-11-05 15:33:57 +0000

    Keep searching for job/instance if multiple job/instance matchers present

--- lint/target_utils.go
+++ lint/target_utils.go
@@ -7,21 +7,26 @@ import (
 )
 
 func checkForMatcher(selector []*labels.Matcher, name string, ty labels.MatchType, value string) error {
+	var result error
+	result = fmt.Errorf("%s selector not found", name)
+
 	for _, matcher := range selector {
 		if matcher.Name != name {
 			continue
 		}
+		if matcher.Type == ty && matcher.Value == value {
+			result = nil
+			break
+		}
 
 		if matcher.Type != ty {
-			return fmt.Errorf("%s selector is %s, not %s", name, matcher.Type, ty)
+			result = fmt.Errorf("%s selector is %s, not %s", name, matcher.Type, ty)
 		}
 
 		if matcher.Value != value {
-			return fmt.Errorf("%s selector is %s, not %s", name, matcher.Value, value)
+			result = fmt.Errorf("%s selector is %s, not %s", name, matcher.Value, value)
 		}
-
-		return nil
 	}
 
-	return fmt.Errorf("%s selector not found", name)
+	return result
 }
